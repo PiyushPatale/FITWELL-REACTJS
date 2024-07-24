67e977c0be8ebe95bfb2791238c52b66ef55575d
PIYUSH PATALE <patale.bhausaheb21b@iiitg.ac.in>
2024-07-24 19:08:21 +0530

    Render deploy

--- fitwell-reactjs/frontend/src/api.js
+++ fitwell-reactjs/frontend/src/api.js
@@ -1,2 +1,3 @@
-export const PUBLIC_SERVER_URL="http://localhost:5001"
+// export const PUBLIC_SERVER_URL="http://localhost:5001"
 // export const PUBLIC_SERVER_URL="https://fitwell-reactjs.onrender.com"
+export const PUBLIC_SERVER_URL="https://fitwell-reactjs-e9uz.onrender.com"
